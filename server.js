11df22309c130be19fed1266f2a6f34049b63862
Sheriff <musasidiabubakar@gmail.com>
2024-10-01 13:51:37 +0100

    added html files

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 import { error } from 'console';
 import http from 'http'
+import fs from 'fs/promises';
+import url, { fileURLToPath } from 'url';
+import path, { dirname }  from 'path';
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const PORT = 8000;
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer(async(req, res)=>{
     // res.setHeader('content-type', 'text/plain')
     try {
         if (req.method == 'GET'){
+            let filepath;
             if (req.url == '/'){
-                res.writeHead(200, {'x-content-type' : 'appliation/json'})
-                res.end(JSON.stringify('at a time no time '));
+                filepath = path.join(__dirname, 'public', 'home.html')
             }    
             else if(req.url == '/about'){
-                res.writeHead(200, {'content-type': 'text/html'})
-                res.end('<h1>ABOUT ME</h1>')
+                filepath = path.join(__dirname, 'public', 'about.html')
             }
             else {
-                res.writeHead(404, {'content-type': 'text/plain'})
-                res.end('NOT FOUND')
+                new Error('NOT FOUND')
             }
+            const data = await fs.readFile(filepath)
+            res.setHeader('content-type', 'text/html')
+            res.write(data)
+            res.end();
+
+
         
         } 
         else if (req.method == 'POST'){
